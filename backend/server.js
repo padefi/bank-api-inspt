b005195923331ebd6187de30465f7872802d513a
padefi <pablo.defilpo@gmail.com>
2023-06-02 14:08:30 -0300

    Conexión a mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
