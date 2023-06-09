271a39e76471e41eab9d3fca41e7ddb1164b87dc
padefi <pablo.defilpo@gmail.com>
2023-06-09 15:00:59 -0300

    Se crean funciones show, close, active Account

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -6,6 +6,31 @@ import User from "../models/userModel.js";
 import { generateAlias, generateCBU } from "../utils/generateAccountInfo.js";
 import { isAdmin, isClient } from "../middlewares/authMiddleware.js";
 
+// @desc    Ver cuentas bancarias
+// @route   GET /api/accounts/show
+// @access  Private
+const getUserAccounts = asyncHandler(async (req, res) => {
+
+    const { userId } = req.body;
+
+    if (isAdmin(req.user) && !userId) {
+        res.status(400);
+        throw new Error('Por favor, complete todos los campos.');
+    }
+
+    const accounts = await Account.find(isAdmin(req.user) ? { accountHolder: userId } : { accountHolder: req.user._id });
+
+    if (!accounts) {
+        res.status(403);
+        throw new Error('Sin autorización. La cuenta no pertenece al usuario logueado.');
+    }
+
+    extendToken(req, res);
+    res.status(201).json({
+        accounts
+    });
+});
+
 // @desc    Crear cuenta bancaria
 // @route   POST /api/accounts/create
 // @access  Private
@@ -81,6 +106,98 @@ const createAccount = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc    Cerrar cuenta bancaria
+// @route   POST /api/accounts/close
+// @access  Private
+const closeAccount = asyncHandler(async (req, res) => {
+
+    const { accountId } = req.body
+
+    // Validación
+    if (!accountId) {
+        res.status(400);
+        throw new Error('Por favor, complete todos los campos.');
+    }
+
+    const account = await Account.findOne({ _id: accountId, accountHolder: req.user._id });
+
+    if (!account) {
+        res.status(403);
+        throw new Error('Sin autorización. La cuenta no pertenece al usuario logueado.');
+    }
+
+    if (!account.isActive) {
+        res.status(400);
+        throw new Error(`La cuenta ${accountId} no se encuentra activa.`);
+    }
+
+    if (account.accountBalance > 0) {
+        res.status(400);
+        throw new Error(`Para poder cerrar la cuenta ${accountId} no debe tener saldo disponible.`);
+    }
+
+    account.isActive = false;
+
+    const updateAccount = await account.save();
+
+    if (updateAccount) {
+        extendToken(req, res);
+        res.status(201).json({
+            message: 'Cuenta cerrada con exito.',
+            _id: updateAccount._id
+
+        });
+    } else {
+        res.status(404);
+        throw new Error('Cuenta no encontrada.');
+    }
+});
+
+// @desc    Activar cuenta bancaria
+// @route   POST /api/accounts/active
+// @access  Private
+const activeAccount = asyncHandler(async (req, res) => {
+
+    const { accountId } = req.body
+
+    // Validación
+    if (!accountId) {
+        res.status(400);
+        throw new Error('Por favor, complete todos los campos.');
+    }
+
+    const account = await Account.findOne({ _id: accountId, accountHolder: req.user._id });
+
+    if (!account) {
+        res.status(403);
+        throw new Error('Sin autorización. La cuenta no pertenece al usuario logueado.');
+    }
+
+    if (account.isActive) {
+        res.status(400);
+        throw new Error(`La cuenta ${accountId} se encuentra activa.`);
+    }
+
+    account.isActive = true;
+
+    const updateAccount = await account.save();
+
+    if (updateAccount) {
+        extendToken(req, res);
+        res.status(201).json({
+            message: 'Cuenta activada con exito.',
+            _id: updateAccount._id
+
+        });
+    } else {
+        res.status(404);
+        throw new Error('Cuenta no encontrada.');
+    }
+});
+
 export {
-    createAccount
+    getUserAccounts,
+    createAccount,
+    closeAccount,
+    activeAccount
 }
--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createAccount } from "../controllers/accountController.js";
+import { activeAccount, closeAccount, createAccount, getUserAccounts } from "../controllers/accountController.js";
 import { protect, isAdmin, isClient } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+router.route('/show').get(protect,getUserAccounts);
 router.route('/create').post(protect,createAccount);
+router.route('/close').post(protect,closeAccount);
+router.route('/active').post(protect,activeAccount);
 
 export default router;
