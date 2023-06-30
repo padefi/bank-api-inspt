52a98191be0e88dfd9b0e35a0076dd751764a19e
padefi <pablo.defilpo@gmail.com>
2023-06-30 13:33:09 -0300

    Se quita navigate

--- frontend/src/utils/useSessionTimeout.js
+++ frontend/src/utils/useSessionTimeout.js
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { logout, userMessage } from '../slices/authSlice';
 import { useLogoutMutation } from "../slices/usersApiSlice";
