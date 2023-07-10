2a15c9cf468af0c29fa953a4b3851a0a85f24a9d
padefi <pablo.defilpo@gmail.com>
2023-07-10 11:55:35 -0300

    Se agrega la paginacion a AccountSummaryPDF

--- frontend/src/components/AccountSummaryPDF.jsx
+++ frontend/src/components/AccountSummaryPDF.jsx
@@ -109,82 +109,88 @@ const styles = StyleSheet.create({
 
 // Componente del resumen de cuenta bancaria
 const AccountSummaryPDF = ({ holder, dateFrom, dateTo, operations }) => {
+  const pageSize = 20;
+  const totalOperations = operations.operationDataArray.length;
+  const totalPages = Math.ceil(totalOperations / pageSize);
+
   return (
     <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.headerContainer}>
-          <View style={styles.headerTitleContainer}>
-            <Image style={styles.headerTitleIcon} src={bank} />
-            <Text style={styles.headerTitle}>Banco INSPT-UTN</Text>
-          </View>
-          <View style={styles.headerTextContainer}>
-            <Text style={styles.headerText}>Resumen de cuenta</Text>
-          </View>
-          <View style={styles.headerDateContainer}>
-            <Text style={styles.headerDate}>Fecha: {new Date().toLocaleDateString()}</Text>
-          </View>
-        </View>
-        <View style={styles.hr} />
-        <View>
-          <View style={styles.section}>
-            <Text>Titular: {holder}</Text>
-            <Text>Cuenta: {operations.accountId.substring(3, 7)} - {operations.accountId.substring(11, 21)}</Text>
-            <Text>Saldo: {operations.accountBalance.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
-          </View>
-          <View style={styles.section}>
-            <View style={styles.datesContainer}>
-              <Text style={styles.datesLabel}>Desde:</Text>
-              <Text style={styles.dateValue}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeZone: 'UTC' }).format(new Date(dateFrom)).replace(/,/g, " -")}</Text>
-              <Text style={styles.datesLabel}>&nbsp;-&nbsp;Hasta:</Text>
-              <Text style={styles.dateValue}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeZone: 'UTC' }).format(new Date(dateTo)).replace(/,/g, " -")}</Text>
+      {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+        <Page size="A4" style={styles.page} key={pageNumber}>
+          <View style={styles.headerContainer}>
+            <View style={styles.headerTitleContainer}>
+              <Image style={styles.headerTitleIcon} src={bank} />
+              <Text style={styles.headerTitle}>Banco INSPT-UTN</Text>
             </View>
-            <View style={styles.hr} />
-            <View style={styles.tableContainer}>
-              <View style={[styles.tableRow, styles.tableHeader]}>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableHeader} fixed>Fecha</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableHeader} fixed>Tipo</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableHeader} fixed>Monto</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableHeader} fixed>Saldo</Text>
-                </View>
+            <View style={styles.headerTextContainer}>
+              <Text style={styles.headerText}>Resumen de cuenta</Text>
+            </View>
+            <View style={styles.headerDateContainer}>
+              <Text style={styles.headerDate}>Fecha: {new Date().toLocaleDateString()}</Text>
+            </View>
+          </View>
+          <View style={styles.hr} />
+          <View>
+            <View style={styles.section}>
+              <Text>Titular: {holder.toUpperCase()}</Text>
+              <Text>Cuenta: {operations.accountId.substring(3, 7)} - {operations.accountId.substring(11, 21)}</Text>
+              <Text>Saldo: {operations.accountBalance.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
+            </View>
+            <View style={styles.section}>
+              <View style={styles.datesContainer}>
+                <Text style={styles.datesLabel}>Desde:</Text>
+                <Text style={styles.dateValue}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeZone: 'UTC' }).format(new Date(dateFrom)).replace(/,/g, " -")}</Text>
+                <Text style={styles.datesLabel}>&nbsp;-&nbsp;Hasta:</Text>
+                <Text style={styles.dateValue}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeZone: 'UTC' }).format(new Date(dateTo)).replace(/,/g, " -")}</Text>
               </View>
-              {operations.operationDataArray.length > 0 ? (
-                operations.operationDataArray.map((operation, index) => (
-                  <View style={styles.tableRow} key={index}>
-                    <View style={styles.tableCell}>
-                      <Text style={styles.tableBody}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeStyle: "short" }).format(new Date(operation.operationDate)).replace(/,/g, " -")}</Text>
-                    </View>
-                    <View style={styles.tableCell}>
-                      <Text style={styles.tableBody}>{operation.type.toUpperCase()}</Text>
-                    </View>
-                    <View style={styles.tableCell}>
-                      <Text style={styles.tableBody}>{operation.amount.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
-                    </View>
-                    <View style={styles.tableCell}>
-                      <Text style={styles.tableBody}>{operation.balance.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
-                    </View>
+              <View style={styles.hr} />
+              <View style={styles.tableContainer}>
+                <View style={[styles.tableRow, styles.tableHeader]}>
+                  <View style={styles.tableCell}>
+                    <Text style={styles.tableHeader} fixed>Fecha</Text>
                   </View>
-                ))
-              ) : (
-                <View style={styles.noResult}>
-                  <Text style={styles.subTitle}>Sin operaciones en el rango indicado</Text>
+                  <View style={styles.tableCell}>
+                    <Text style={styles.tableHeader} fixed>Tipo</Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text style={styles.tableHeader} fixed>Monto</Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text style={styles.tableHeader} fixed>Saldo</Text>
+                  </View>
                 </View>
-              )}
+                {operations.operationDataArray.length > 0 ? (
+                  operations.operationDataArray.slice((pageNumber - 1) * pageSize, pageNumber * pageSize).map((operation, index) => (
+                    <View style={styles.tableRow} key={index}>
+                      <View style={styles.tableCell}>
+                        <Text style={styles.tableBody}>{new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeStyle: "short" }).format(new Date(operation.operationDate)).replace(/,/g, " -")}</Text>
+                      </View>
+                      <View style={styles.tableCell}>
+                        <Text style={styles.tableBody}>{operation.type.toUpperCase()}</Text>
+                      </View>
+                      <View style={styles.tableCell}>
+                        <Text style={styles.tableBody}>{operation.amount.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
+                      </View>
+                      <View style={styles.tableCell}>
+                        <Text style={styles.tableBody}>{operation.balance.toLocaleString("es-AR", { style: "currency", currency: operations.currency.acronym })}</Text>
+                      </View>
+                    </View>
+                  ))
+                ) : (
+                  <View style={styles.noResult}>
+                    <Text style={styles.subTitle}>Sin operaciones en el rango indicado</Text>
+                  </View>
+                )}
+              </View>
             </View>
           </View>
-        </View>
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerText} render={({ pageNumber, totalPages }) => (
-            `Página: ${pageNumber} de ${totalPages}`
-          )} fixed />
-        </View>
-      </Page>
+          <View style={styles.footerContainer}>
+            <Text style={styles.footerText} render={({ pageNumber, totalPages }) => (
+              `Página: ${pageNumber} de ${totalPages}`
+            )} fixed />
+          </View>
+        </Page>
+      ))}
     </Document>
   );
 };
--- frontend/src/views/AccountSummary.jsx
+++ frontend/src/views/AccountSummary.jsx
@@ -72,12 +72,11 @@ const AccountSumarry = () => {
     />).toBlob();
 
     const url = URL.createObjectURL(blob);
-    window.open(url, '_blank');
-    /* const link = document.createElement('a');
+    const link = document.createElement('a');
     link.href = url;
     link.download = dataAccountOperations.accountId + '.pdf';
     link.click();
-    URL.revokeObjectURL(url); */
+    URL.revokeObjectURL(url);
   }
 
   return (
